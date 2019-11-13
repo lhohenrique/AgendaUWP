b8c37d1b70a006504a765ffcee6e49a3202567a5
ehms <ehms@cin.ufpe.br>
2019-11-13 18:11:24 -0300

    UC1 - Configure a prism project. Creating a main page. Adding the components in the screen. Creating folder structure.

---
+++ AgendaUWP/AgendaUWP/ModelViews/MainPageModelView.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaUWP.ModelViews
+{
+    class MainPageModelView : ViewModelBase
+    {
+
+    }
+}
---
+++ AgendaUWP/AgendaUWP/Models/PageTokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaUWP.Models
+{
+    class PageTokens
+    {
+        public const string MainPage = "Main";
+    }
+}
