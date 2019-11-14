8d625323bb293da4efd2de82413881aa2ea41385
ehms <ehms@cin.ufpe.br>
2019-11-14 16:44:48 -0300

    versioning current projetc

---
+++ AgendaUWP/AgendaUWP/Models/Contact.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaUWP.Models
+{
+    class Contact : BindableBase
+    {
+        #region properties
+        private string _fullname;
+        public string FullName
+        {
+            get { return _fullname; }
+            set { SetProperty(ref _fullname, value); }
+        }
+
+        private string _nickname;
+        public string NickName
+        {
+            get { return _nickname; }
+            set { SetProperty(ref _nickname, value); }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { SetProperty(ref _age, value); }
+        }
+
+        private int _phone;
+        public int Phone
+        {
+            get { return _phone; }
+            set { SetProperty(ref _phone, value); }
+        }
+        #endregion
+    }
+}
