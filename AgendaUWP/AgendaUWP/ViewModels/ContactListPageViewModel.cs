1460c535008e6b58bf8820bdcc089e786450a4b4
ehms <ehms@cin.ufpe.br>
2019-11-20 14:07:50 -0300

    Detail a contact on item click

--- AgendaUWP/AgendaUWP/ViewModels/ContactListPageViewModel.cs
+++ AgendaUWP/AgendaUWP/ViewModels/ContactListPageViewModel.cs
@@ -1,7 +1,11 @@
-﻿using Prism.Windows.Mvvm;
+﻿using AgendaUWP.Models;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
 using Service.Contact;
 using Service.Model;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace AgendaUWP.ViewModels
@@ -9,19 +13,52 @@ namespace AgendaUWP.ViewModels
     public class ContactListPageViewModel : ViewModelBase
     {
         private readonly IContactService<Contact> _contactService;
+        private INavigationService _navigationService;
 
-        public ContactListPageViewModel(IContactService<Contact> contactService)
+        private ObservableCollection<ContactItemsGroup> data;
+        public ObservableCollection<ContactItemsGroup> Data
         {
-            this._contactService = contactService;
-            GetData();
-        }
-
-        private void GetData()
-        {
-            var contacts = _contactService.GetContacts();
-            GroupedContactItems = contacts.GroupBy(m => m.FullName[0] , (key, list) => new ContactItemsGroup(key, list));
+            get { return data; }
+            set { SetProperty(ref data, value); }
         }
 
         public IEnumerable<ContactItemsGroup> GroupedContactItems { get; private set; }
+
+
+        public DelegateCommand<Contact> EditCommand { get; private set; }
+
+        #region initializers
+        private void InitilizeCommands()
+        {
+            EditCommand = new DelegateCommand<Contact>(EditContact);
+        }
+        #endregion
+
+        private void EditContact(Contact contact)
+        {
+            _navigationService.Navigate(PageTokens.DetailContactPage, contact);
+        }
+
+        public ContactListPageViewModel(IContactService<Contact> contactService, INavigationService navigationService)
+        {
+            this._contactService = contactService;
+            this._navigationService = navigationService;
+            Data = GetData();
+            InitilizeCommands();
+        }
+
+        private ObservableCollection<ContactItemsGroup> GetData()
+        {
+            var contacts = _contactService.GetContacts();
+            if (contacts != null && contacts.Count() > 0)
+            {
+                GroupedContactItems = contacts.GroupBy(m => m.FullName[0], (key, list) => new ContactItemsGroup(key, list));
+                return new ObservableCollection<ContactItemsGroup>(GroupedContactItems);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
---
+++ AgendaUWP/AgendaUWP/ViewModels/DetailContactPageViewModel.cs
@@ -0,0 +1,75 @@
+﻿using AgendaUWP.Models;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using Service;
+using Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaUWP.ViewModels
+{
+    public class DetailContactPageViewModel : ViewModelBase
+    {
+        #region properties
+        private INavigationService navigationService;
+        private Contact selectedContact;
+
+        public Contact SelectedContact
+        {
+            get { return selectedContact; }
+            set { SetProperty(ref selectedContact, value); }
+        }
+        #endregion
+
+        
+        
+
+        #region Constructor
+        public DetailContactPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+            InitilizeCommands();
+        }
+        #endregion
+
+        #region overrides
+        public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(e, viewModelState);
+
+            if (e.Parameter != null && e.Parameter is Contact)
+            {
+                var contact = (Contact)e.Parameter;
+                SelectedContact = contact;
+            }
+
+        }
+
+
+        public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+        {
+            base.OnNavigatingFrom(e, viewModelState, suspending);
+        }
+        #endregion
+
+        #region commands
+        public DelegateCommand CancelCommand { get; private set; }
+        #endregion
+
+        #region initializers
+        private void InitilizeCommands()
+        {
+            CancelCommand = new DelegateCommand(Cancel);
+        }
+        #endregion
+
+        private void Cancel()
+        {
+            navigationService.Navigate(PageTokens.MainPage, false);
+        }
+    }
+}
