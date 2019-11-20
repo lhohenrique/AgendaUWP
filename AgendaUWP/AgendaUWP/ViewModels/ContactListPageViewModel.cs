d30298a42ccce42b6e2e91945cb71fcddbd98c57
ehms <ehms@cin.ufpe.br>
2019-11-20 14:38:46 -0300

    Remove blanking spaces and an empty collection when there is no item stored

--- AgendaUWP/AgendaUWP/ViewModels/ContactListPageViewModel.cs
+++ AgendaUWP/AgendaUWP/ViewModels/ContactListPageViewModel.cs
@@ -57,7 +57,7 @@ namespace AgendaUWP.ViewModels
             }
             else
             {
-                return null;
+                return new ObservableCollection<ContactItemsGroup>();
             }
         }
     }
